954f527894e72712252973a6937719b6044dd059
Erebus54 <patrick.schnurbusch@gmail.com>
2020-10-25 00:24:45 -0400

    focus on NLP

--- ExploratoryAnalysis.R
+++ ExploratoryAnalysis.R
@@ -1,12 +1,10 @@
 library(data.table)
 library(dplyr)
-
+library(lubridate)
+library(highcharter)
+library(plotly)
 #source
 #https://www.kaggle.com/andrewmvd/cyberpunk-2077#
 
 comment_replies <- data.table::fread("./datasets/comment_replies.csv", encoding = "UTF-8", sep = ",", data.table = F)
 comments <- data.table::fread("./datasets/comments.csv", encoding = "UTF-8", sep = ",", data.table = F)
-google_trends_cyberpunk <- read.csv("./datasets/google_trends_cyberpunk.csv", encoding = "UTF-8")
-videos <- read.csv("./datasets/videos.csv", encoding = "UTF-8")
-
-
