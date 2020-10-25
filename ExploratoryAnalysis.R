001ca3b960bef7ec24df4fea8d06c6709632d2f5
Erebus54 <patrick.schnurbusch@gmail.com>
2020-10-24 21:41:01 -0400

    changed file reader to include utf-8 encoding schema

--- ExploratoryAnalysis.R
+++ ExploratoryAnalysis.R
@@ -6,7 +6,7 @@ library(dplyr)
 
 comment_replies <- data.table::fread("./datasets/comment_replies.csv", encoding = "UTF-8", sep = ",", data.table = F)
 comments <- data.table::fread("./datasets/comments.csv", encoding = "UTF-8", sep = ",", data.table = F)
-google_trends_cyberpunk <- read.csv("./datasets/google_trends_cyberpunk.csv")
-videos <- read.csv("./datasets/videos.csv")
+google_trends_cyberpunk <- read.csv("./datasets/google_trends_cyberpunk.csv", encoding = "UTF-8")
+videos <- read.csv("./datasets/videos.csv", encoding = "UTF-8")
 
 
