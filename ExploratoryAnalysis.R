91ace7c8aa3e182f294041806f9dc6523100ac43
Erebus54 <patrick.schnurbusch@gmail.com>
2020-11-20 16:15:18 -0500

    Added files via upload

--- ExploratoryAnalysis.R
+++ ExploratoryAnalysis.R
@@ -10,3 +10,11 @@ google_trends_cyberpunk <- read.csv("./datasets/google_trends_cyberpunk.csv", en
 videos <- read.csv("./datasets/videos.csv", encoding = "UTF-8")
 
 
+
+
+
+install.packages("tuber")
+library(tuber) # youtube API
+library(magrittr) # Pipes %>%, %T>% and equals(), extract().
+library(tidyverse) # all tidyverse packages
+library(purrr) # package for iterating/extracting data
