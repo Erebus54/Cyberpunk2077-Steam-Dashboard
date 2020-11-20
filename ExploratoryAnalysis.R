c1f170abb29889609383020df13fca56610f9e86
Erebus54 <patrick.schnurbusch@gmail.com>
2020-11-20 16:17:11 -0500

    Added files via upload

--- ExploratoryAnalysis.R
+++ ExploratoryAnalysis.R
@@ -8,12 +8,9 @@ library(plotly)
 
 comment_replies <- data.table::fread("./datasets/comment_replies.csv", encoding = "UTF-8", sep = ",", data.table = F)
 comments <- data.table::fread("./datasets/comments.csv", encoding = "UTF-8", sep = ",", data.table = F)
-<<<<<<< HEAD
+
 google_trends_cyberpunk <- read.csv("./datasets/google_trends_cyberpunk.csv", encoding = "UTF-8")
 videos <- read.csv("./datasets/videos.csv", encoding = "UTF-8")
-
-
-
 
 
 install.packages("tuber")
@@ -21,5 +18,4 @@ library(tuber) # youtube API
 library(magrittr) # Pipes %>%, %T>% and equals(), extract().
 library(tidyverse) # all tidyverse packages
 library(purrr) # package for iterating/extracting data
-=======
->>>>>>> 1aab3717e69e7166f0ed7ecde3d21321e8a4df34
+
